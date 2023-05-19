91d9b1877ed7e3db513a3bcacad8436ef9a6b815
Lux <126984176+BONAPARTER@users.noreply.github.com>
2023-05-19 19:20:34 +0300

    Add files via upload

---
+++ UnitYes.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Yes/Yes.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitYes
+{
+	TEST_CLASS(UnitYes)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			const auto [max, min, devider] = FindDevider(1, 20);
+			Assert::AreEqual(20, max);
+			Assert::AreEqual(12, min);
+			Assert::AreEqual(6, devider);
+		}
+	};
+}
